6ab72dd1fc7bf1a6eccc1ab41d5b532fa2adabab
Abhishek-Kartik <abhishek123cu@gmail.com>
2026-02-19 21:59:30 +0530

    forgot password ui fix

--- product-exchange-ui/src/app/pages/forgot-password/forgot-password.component.ts
+++ product-exchange-ui/src/app/pages/forgot-password/forgot-password.component.ts
@@ -35,12 +35,37 @@ export class ForgotPasswordComponent {
   ) {}
 
   onCodeCompleted(token: string) {
-    this.resetPasswordRequest.otp = token;
-    this.isOtpVerified = true;
+    this.confirmAccount(token);
   }
 
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
 
-  forgotPasswordOtp(email: string) {
+  backToLoginPage() {
+    this.router.navigate(['login']);
+  }
+
+  private confirmAccount(token: string) {
+    this.authService.confirm({ token }).subscribe({
+      next: () => {
+        this.message =
+          'Email verified successfully. You can now reset your password.';
+        this.submitted = true;
+        this.isOkay = true;
+        this.resetPasswordRequest.otp = token;
+        this.isOtpVerified = true;
+      },
+      error: () => {
+        this.message = 'Token has been expired or invalid';
+        this.submitted = true;
+        this.isOkay = false;
+      },
+    });
+  }
+
+  forgotPasswordOtp() {
+    this.errorMsg = [];
     this.authService
       .forgotPassword({ body: this.forgotPasswordRequest })
       .subscribe({
@@ -50,8 +75,12 @@ export class ForgotPasswordComponent {
           this.startCountdown(30);
         },
         error: (err) => {
-          console.error(err);
-          this.errorMsg.push('Something went wrong');
+          console.log(err);
+          if (err.error.validationErrors) {
+            this.errorMsg = err.error.validationErrors;
+          } else {
+            this.errorMsg.push(err.error.error);
+          }
         },
       });
   }
@@ -80,19 +109,13 @@ export class ForgotPasswordComponent {
     }, 1000);
   }
 
-  togglePassword() {
-    this.showPassword = !this.showPassword;
-  }
-
-  backToLoginPage() {
-    this.router.navigate(['login']);
-  }
-
   resetPassword() {
     this.authService
       .resetPassword({ body: this.resetPasswordRequest })
       .subscribe({
-        next: () => {},
+        next: () => {
+          this.router.navigate(['login']);
+        },
         error: () => {},
       });
   }
--- product-exchange/src/main/java/com/abhishek/product_exchange/auth/ForgotPasswordRequest.java
+++ product-exchange/src/main/java/com/abhishek/product_exchange/auth/ForgotPasswordRequest.java
@@ -1,12 +1,15 @@
 package com.abhishek.product_exchange.auth;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ForgotPasswordRequest {
+
     @Email
-    @NotNull
+    @NotEmpty(message = "Email is mandatory")
+    @NotNull(message = "Email is mandatory")
     private String email;
 }

b64e98cbf1a7999f41954297bba877c48495676c
Abhishek-Kartik <abhishek123cu@gmail.com>
2026-02-22 17:27:21 +0530

    Navbar Fixed

--- product-exchange-ui/src/app/modules/product/components/menu/menu.component.ts
+++ product-exchange-ui/src/app/modules/product/components/menu/menu.component.ts
@@ -16,16 +16,6 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
    this.username = this.tokenService.getUsername();
-    const linkColor = document.querySelectorAll('.nav-link');
-    linkColor.forEach((link) => {
-      if (window.location.href.endsWith(link.getAttribute('href') || '')) {
-        link.classList.add('active');
-      }
-      link.addEventListener('click', () => {
-        linkColor.forEach((l) => l.classList.remove('active'));
-        link.classList.add('active');
-      });
-    });
   }
 
   logout() {
